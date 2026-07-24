cf978fd609640810b118b2924adeeb31ff2eb10e
Veysel Bayar <vyslbyrx@gmail.com>
2026-07-24 14:25:23 +0200

    feat: add AGB and Datenschutz API endpoints

---
+++ src/api/agb/controllers/agb.ts
@@ -0,0 +1,7 @@
+/**
+ * agb controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::agb.agb');
---
+++ src/api/agb/routes/agb.ts
@@ -0,0 +1,7 @@
+/**
+ * agb router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::agb.agb');
